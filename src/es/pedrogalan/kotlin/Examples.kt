047b3be84fb00d21c66d96c74d0ebc0d3b62c1d8
Pedro Galan <pedro.galan@piksel.com>
2020-04-04 13:17:43 +0100

    Create function println

--- src/es/pedrogalan/kotlin/Examples.kt
+++ src/es/pedrogalan/kotlin/Examples.kt
@@ -2,9 +2,11 @@ package es.pedrogalan.kotlin
 
 fun main(args: Array<String>) {
     val message = args[0]
-    println(message.greeting())
+    message.greeting().println()
 }
 
 private fun String.greeting(): String {
     return "Hello $this"
 }
+
+private fun String.println() = println(this)
